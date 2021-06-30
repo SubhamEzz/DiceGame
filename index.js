59ae817acd625ff5a90e767cc641a2f8b34fb2c4
SubhamEzz <kumarsubh2000@gmail.com>
2021-06-30 07:55:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomno1= Math.floor(Math.random()*6)+1;
+var randomImg = "../dice"+randomno1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImg);
+
+var randomno2= Math.floor(Math.random()*6)+1;
+var randomImg2 = "../dice"+randomno2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImg2);
+
+if(randomno1>randomno2){
+  document.querySelector("h1").innerHTML="🐱‍💻 PLAYER 1 WINS!"
+
+}
+else if(randomno1<randomno2){
+  document.querySelector("h1").innerHTML="PLAYER 2 WINS! 🐱‍💻"
+  document.querySelector("h1").font
+}
+else{
+  document.querySelector("h1").innerHTML="‍DRAW !! 💻"
+}
